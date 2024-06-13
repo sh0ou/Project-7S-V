1bcd5f9e63bb67a91ea0a0c5a3c31bdee2316d51
sh0u <47475540+sh0ou@users.noreply.github.com>
2024-06-13 17:51:36 +0900

    会話イベントデータ用スクリプトの作成
    - 一部カスタムクラスの初期化が行えない不具合あり

---
+++ Assets/Contents/Scripts/GreenFoxEngine/Runtime/CommonEventData.cs
@@ -0,0 +1,85 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDK3.Data;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace sh0uRoom.GFE
+{
+    public class CommonEventData : UdonSharpBehaviour
+    {
+        public string eventName;
+        // public List<CommonEventAction> actionList;
+        public CommonEventAction[] actions;
+    }
+
+    [System.Serializable]
+    public class CommonEventAction
+    {
+        public EventActionType actionType = EventActionType.Talk;
+        public TalkAction talkAction;
+        public ChooseAction chooseAction;
+        public ParameterChangeAction paramChangeAction;
+        public ParameterBranchAction paramBranchAction;
+
+        public CommonEventAction()
+        {
+            talkAction = new TalkAction();
+            chooseAction = new ChooseAction();
+            paramChangeAction = new ParameterChangeAction();
+            paramBranchAction = new ParameterBranchAction();
+        }
+    }
+
+    public enum EventActionType
+    {
+        Talk,
+        Choose,
+        ParameterChange,
+        ParameterBranch,
+        CallEvent
+    };
+
+    [System.Serializable]
+    public class TalkAction
+    {
+        public string name;
+        public string text;
+        public string windowColor;
+    }
+
+    [System.Serializable]
+    public class ChooseAction
+    {
+        public string[] text;
+        public int defaultId;
+        public string[] eventNames;
+    }
+
+    [System.Serializable]
+    public class ParameterChangeAction
+    {
+        public string inputId;
+        public string outputId;
+    }
+
+    [System.Serializable]
+    public class ParameterBranchAction
+    {
+        public string refId;
+        public string ifValue;
+        public IfSign ifSign;
+        public string trueEventName;
+        public string falseEventName;
+    }
+
+    public enum IfSign
+    {
+        Equal = 0,
+        NotEqual = 1,
+        Greater = 2,
+        Less = 3,
+        GreaterEqual = 4,
+        LessEqual = 5
+    }
+}
---
+++ Assets/Contents/Scripts/GreenFoxEngine/Runtime/JsonToCEV.cs
@@ -0,0 +1,137 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using VRC.SDK3.Data;
+
+namespace sh0uRoom.GFE
+{
+    public class JsonToCEV : UdonSharpBehaviour
+    {
+        [SerializeField] private TextAsset jsonFile;
+        [SerializeField] private CommonEventData targetCEV;
+        void Start()
+        {
+            Convert2CEV(jsonFile.text);
+        }
+
+        public void Convert2CEV(string json)
+        {
+            Debug.Log(json);
+            if (VRCJson.TryDeserializeFromJson(json, out DataToken deserializedObject))
+            {
+                if (deserializedObject.DataDictionary.GetKeys().Count == 0)
+                {
+                    Debug.LogError("Failed to deserialize json");
+                    return;
+                }
+                else
+                {
+                    //talkキーの内容をCEVに反映
+                    foreach (var keyArray in deserializedObject.DataDictionary.GetKeys().ToArray())
+                    {
+                        if (keyArray == "action")
+                        {
+                            var actions = deserializedObject.DataDictionary[keyArray].DataList;
+                            Debug.Log($"actions: {actions.Count}");
+                            foreach (var action in actions)
+                            {
+                                var actionDict = action.DataDictionary;
+                                var commonEventAction = new CommonEventAction();
+                                if (actionDict.ContainsKey("talk"))
+                                {
+                                    commonEventAction.actionType = EventActionType.Talk;
+                                    commonEventAction.talkAction = ExtractTalkAction(actionDict["talk"].DataDictionary);
+                                }
+                                else if (actionDict.ContainsKey("choose"))
+                                {
+                                    commonEventAction.actionType = EventActionType.Choose;
+                                    commonEventAction.chooseAction = ExtractChooseAction(actionDict["choose"].DataDictionary);
+                                }
+                                else if (actionDict.ContainsKey("paramChange"))
+                                {
+                                    commonEventAction.actionType = EventActionType.ParameterChange;
+                                    commonEventAction.paramChangeAction = ExtractParameterChangeAction(actionDict["paramChange"].DataDictionary);
+                                }
+                                else if (actionDict.ContainsKey("paramBranch"))
+                                {
+                                    commonEventAction.actionType = EventActionType.ParameterBranch;
+                                    commonEventAction.paramBranchAction = ExtractParameterBranchAction(actionDict["paramBranch"].DataDictionary);
+                                }
+                                AddActionToArray(ref targetCEV.actions, commonEventAction);
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Debug.LogError("Failed to deserialize json");
+            }
+        }
+
+        private void AddActionToArray(ref CommonEventAction[] array, CommonEventAction newAction)
+        {
+            var newArray = new CommonEventAction[array.Length + 1];
+            for (int i = 0; i < array.Length; i++)
+            {
+                newArray[i] = array[i];
+            }
+            newArray[array.Length] = newAction;
+            array = newArray;
+        }
+
+        private TalkAction ExtractTalkAction(DataDictionary talkDict)
+        {
+            TalkAction talkAction = new TalkAction();
+            talkAction.name = talkDict["name"].String;
+            talkAction.text = talkDict["text"].String;
+            talkAction.windowColor = talkDict["windowColor"].String;
+            return talkAction;
+        }
+
+        private ChooseAction ExtractChooseAction(DataDictionary chooseDict)
+        {
+            var textList = chooseDict["text"].DataList;
+            var eventList = chooseDict["event"].DataList;
+
+            var textArray = new string[textList.Count];
+            for (int i = 0; i < textList.Count; i++)
+            {
+                textArray[i] = textList[i].String;
+            }
+
+            var eventArray = new string[eventList.Count];
+            for (int i = 0; i < eventList.Count; i++)
+            {
+                eventArray[i] = eventList[i].String;
+            }
+
+            var chooseAction = new ChooseAction();
+            chooseAction.text = textArray;
+            chooseAction.defaultId = chooseDict["defaultId"].Int;
+            chooseAction.eventNames = eventArray;
+            return chooseAction;
+        }
+
+        private ParameterChangeAction ExtractParameterChangeAction(DataDictionary paramChangeDict)
+        {
+            var paramChangeAction = new ParameterChangeAction();
+            paramChangeAction.inputId = paramChangeDict["inputId"].String;
+            paramChangeAction.outputId = paramChangeDict["outputId"].String;
+            return paramChangeAction;
+        }
+
+        private ParameterBranchAction ExtractParameterBranchAction(DataDictionary paramBranchDict)
+        {
+            var paramBranchAction = new ParameterBranchAction();
+            paramBranchAction.refId = paramBranchDict["refId"].String;
+            paramBranchAction.ifValue = paramBranchDict["ifValue"].String;
+            paramBranchAction.ifSign = (IfSign)System.Enum.Parse(typeof(IfSign), paramBranchDict["ifSign"].String);
+            paramBranchAction.trueEventName = paramBranchDict["trueEvent"].String;
+            paramBranchAction.falseEventName = paramBranchDict["falseEvent"].String;
+            return paramBranchAction;
+        }
+    }
+}
