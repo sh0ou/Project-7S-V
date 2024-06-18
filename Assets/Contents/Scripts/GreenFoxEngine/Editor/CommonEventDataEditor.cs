f5f53269d635e63b805789676988f70efceaf272
sh0u <47475540+sh0ou@users.noreply.github.com>
2024-06-18 12:39:54 +0900

    Update CommonEventDataEditor

--- Assets/Contents/Scripts/GreenFoxEngine/Editor/CommonEventDataEditor.cs
+++ Assets/Contents/Scripts/GreenFoxEngine/Editor/CommonEventDataEditor.cs
@@ -10,7 +10,7 @@ namespace sh0uRoom.GFE
     [CustomEditor(typeof(CommonEventData))]
     public class CommonEventDataEditor : Editor
     {
-        [SerializeField] private VisualTreeAsset rootAsset;
+        [SerializeField] private VisualTreeAsset actionRootAsset;
         [SerializeField] private VisualTreeAsset actionItemAsset;
         [SerializeField] private VisualTreeAsset actionTalkAsset;
         [SerializeField] private VisualTreeAsset actionChooseAsset;
@@ -33,34 +33,37 @@ namespace sh0uRoom.GFE
             });
             root.Add(imguiContainer);
 
-            rootAsset.CloneTree(root);
+            var actionRoot = actionRootAsset.CloneTree();
+            root.Add(actionRoot);
+            
+            // var actionListView = root.Q<ListView>();
+            // Debug.Log($"actionListView: {actionListView.name}");
 
-            //アクションリストの要素数を出力
-            var actionListView = root.Q<ListView>();
-            Debug.Log($"actionListView: {actionListView.name}");
-            var commonEventData = target as CommonEventData;
-            if (actionListView != null)
-            {
-                //各アクションの要素を生成
-                foreach (var action in commonEventData.actions)
-                {
-                    var actionType = action.actionType;
+            // var commonEventData = target as CommonEventData;
+            // if (actionListView != null)
+            // {
+            //     //各アクションの要素を生成
+            //     foreach (var action in commonEventData.actions)
+            //     {
+            //         var actionType = action.actionType;
 
-                    switch (actionType)
-                    {
-                        case EventActionType.Talk:
-                            break;
-                        case EventActionType.Choose:
-                            break;
-                        case EventActionType.ParameterBranch:
-                            break;
-                        case EventActionType.ParameterChange:
-                            break;
-                        default:
-                            break;
-                    }
-                }
-            }
+            //         switch (actionType)
+            //         {
+            //             case EventActionType.Talk:
+            //                 //他のアクションを非表示
+            //                 Debug.Log($"target: {target.name}");
+            //                 break;
+            //             case EventActionType.Choose:
+            //                 break;
+            //             case EventActionType.ParameterBranch:
+            //                 break;
+            //             case EventActionType.ParameterChange:
+            //                 break;
+            //             default:
+            //                 break;
+            //         }
+            //     }
+            // }
 
 
             // var talkView = itemView.Q<ListView>();
