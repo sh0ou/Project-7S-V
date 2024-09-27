b70126f5d1fa23dbe7b96a0003fbeb07ada7be2a
sh0u <47475540+sh0ou@users.noreply.github.com>
2024-09-27 14:26:40 +0900

    PlayerStatusView作成

---
+++ Assets/Contents/Scripts/Base/Runtime/PlayerStatusView.cs
@@ -0,0 +1,17 @@
+using UdonSharp;
+using UnityEngine;
+using UnityEngine.UIElements;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace sh0uRoom.PJ7S
+{
+    public class PlayerStatusView : UdonSharpBehaviour
+    {
+        private void Start()
+        {
+            
+        }
+    }
+
+}
