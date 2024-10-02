358cc3d2be31f329db8dd1aecb3ee5b2f8b43938
sh0u <47475540+sh0ou@users.noreply.github.com>
2024-10-03 01:53:56 +0900

    ゲージ遅延追従スクリプトの追加

---
+++ Assets/Contents/Scripts/Base/Runtime/FollowGaugeHUD.cs
@@ -0,0 +1,39 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace sh0uRoom.PJ7S
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    /// <summary>
+    /// ゲージUIに追従するクラス
+    /// </summary>
+    public class FollowGaugeHUD : UdonSharpBehaviour
+    {
+        [SerializeField] private Image followGaugeImage;
+        [SerializeField] private Image myGaugeImage;
+        [SerializeField] private float leapSpeed = 1.0f;
+        private float thresholdValue = 0.005f;
+
+        public override void PostLateUpdate()
+        {
+            // 双方のfillAmountの差が閾値より大きい場合
+            if (Mathf.Abs(followGaugeImage.fillAmount - myGaugeImage.fillAmount) > thresholdValue)
+            {
+                myGaugeImage.fillAmount = GetLeapGaugeValue();
+            }
+            else
+            {
+                myGaugeImage.fillAmount = followGaugeImage.fillAmount;
+            }
+        }
+
+        /// <summary>
+        /// 減衰後の値を取得する
+        /// </summary>
+        private float GetLeapGaugeValue() => Mathf.Lerp(myGaugeImage.fillAmount, followGaugeImage.fillAmount, Time.deltaTime * leapSpeed);
+    }
+}
