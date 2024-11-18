0507661050ecd00d7d07fff8e7943e83bcc343f5
sh0u <47475540+sh0ou@users.noreply.github.com>
2024-11-18 15:06:19 +0900

    敵制御スクリプトの作成

---
+++ Assets/Contents/Scripts/Base/Runtime/Object/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDK3.Data;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace sh0uRoom.PJ7S
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class Enemy : UdonSharpBehaviour
+    {
+        
+        private const string DEBUG_HEAD = "[<color=yellow>Enemy</color>]";
+    }
+
+    public enum EnemyState
+    {
+        Idle,   // 待機
+        Scout,  // 索敵
+        Chase,  // 追跡
+        Engage  // 交戦
+    }
+}
