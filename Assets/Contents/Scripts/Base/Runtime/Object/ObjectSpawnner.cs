27b2d8b18d2a0672cee054da8c7ac0d7d00bb6f5
sh0u <47475540+sh0ou@users.noreply.github.com>
2024-11-18 22:01:15 +0900

    ObjectSpawnnerの作成

---
+++ Assets/Contents/Scripts/Base/Runtime/Object/ObjectSpawnner.cs
@@ -0,0 +1,47 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace sh0uRoom.PJ7S
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+    public class ObjectSpawnner : UdonSharpBehaviour
+    {
+        private void Start()
+        {
+            if (Utilities.IsValid(targetObject))
+            {
+                // 対象のオブジェクトを表示
+                targetObject.SetActive(true);
+            }
+        }
+
+        private void Update()
+        {
+            if (targetObject.activeSelf) return;
+
+            spawnIntervalTimer += Time.deltaTime;
+            if (spawnIntervalTimer >= spawnInterval)
+            {
+                spawnIntervalTimer = 0f;
+                SpawnObject();
+            }
+        }
+
+        public void SpawnObject()
+        {
+            if (Utilities.IsValid(targetObject))
+            {
+                // 対象のオブジェクトを表示する
+                targetObject.SetActive(true);
+                targetObject.transform.SetPositionAndRotation(spawnPoint.position, spawnPoint.rotation);
+            }
+        }
+
+        [SerializeField] private GameObject targetObject;
+        [SerializeField] private Transform spawnPoint;
+        [SerializeField] private float spawnInterval = 30f;
+        [SerializeField] private float spawnIntervalTimer = 0f;
+    }
+}
